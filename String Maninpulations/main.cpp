75f05b4f42c0ecf0b86d135215b6b5813c864c18
Elmar <elmar465@gmail.com>
2023-05-03 12:28:42 -0400

    Test2

--- String Maninpulations/main.cpp
+++ String Maninpulations/main.cpp
@@ -6,9 +6,32 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+        //std::strlean : Find the length of a string
+        // real arrays and those decayed into pointers
+            const char message1 [] {"The skye is blue"};
+            
+            //Array decays into pointer when we use const char
+    const char* message2 {"The sky is blue"};
+    std::cout <<"message1" << message1 << std::endl;
+    
+    //strlen ignores null characters
+    std::cout << "strlen(message1) : " << std::strlen(message1) << std::endl;
+    
+    //Inclueds the null character
+    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl;
+    
+    //strlen still works with decayed arrays
+    std::cout << "strlen(message2) : " << std::strlen(message2) << std::endl;
+            
+    //Prints size of pointer
+    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;
+    
+    
+        
+    
     return 0;
 }
