ae16acd181f800063c57c51f4d398672b1cfc49f
Elmar <elmar465@gmail.com>
2023-05-03 12:08:36 -0400

    Initial Commit

---
+++ String Maninpulations/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  String Maninpulations
+//
+//  Created by Elmar Jafarli on 03.05.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
